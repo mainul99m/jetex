be9bcdea306a4ed1646f4f93cf8cfd8f3d7f27e1
Kazi Mainul Islam <mainul99m@gmail.com>
2021-03-30 15:17:13 +0600

    Add map and location for android and ios

--- lib/ui/screens/delivery/request_delivery_screen.dart
+++ lib/ui/screens/delivery/request_delivery_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:jetex_app/ui/widgets/back_button.dart';
 import 'package:jetex_app/ui/widgets/bottom_sun_button.dart';
@@ -17,8 +19,10 @@ class RequestDeliveryScreen extends StatefulWidget {
 
 class _RequestDeliveryScreenState extends State<RequestDeliveryScreen> {
   bool _isExpanded = true;
-
+  String _pickedAddress = '';
+  LatLng _lastMapPosition;
   Completer<GoogleMapController> _controller = Completer();
+
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -36,9 +40,48 @@ class _RequestDeliveryScreenState extends State<RequestDeliveryScreen> {
               mapType: MapType.normal,
               initialCameraPosition: _kGooglePlex,
               onCameraMove: _onCameraMove,
+              onCameraIdle: _onCameraIdle,
               onMapCreated: (GoogleMapController controller) {
                 _controller.complete(controller);
               },
+            ),
+            Center(
+              child: Icon(
+                Icons.location_on_sharp,
+                size: 30,
+                color: ColorPalette.sun,
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: _size.height * 0.35 + 20,
+              child: Container(
+                height: 50,
+                width: 60,
+                child: Material(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10)
+                  ),
+                  color: Colors.white,
+                  child: InkWell(
+                    onTap: _getCurrentLocation,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        bottomLeft: Radius.circular(10)
+                    ),
+                    child: Center(
+                      child: RotatedBox(
+                        quarterTurns: 3,
+                        child: Icon(
+                          CustomIcons.location_arrow,
+                          color: ColorPalette.darkPurple,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
@@ -228,7 +271,7 @@ class _RequestDeliveryScreenState extends State<RequestDeliveryScreen> {
             bottom: 18
           ),
           child: Text(
-            'Confirm Adress',
+            'Confirm Address',
             style: TextStyle(
               color: ColorPalette.darkPurple,
               fontFamily: 'HelveticaNeue',
@@ -260,7 +303,7 @@ class _RequestDeliveryScreenState extends State<RequestDeliveryScreen> {
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     child: Text(
-                      'Dilara Aliyev 253,',
+                      _pickedAddress,
                       style: TextStyle(
                         fontFamily: 'HelveticaNeue',
                         fontSize: 20,
@@ -296,9 +339,77 @@ class _RequestDeliveryScreenState extends State<RequestDeliveryScreen> {
 
 
   void _onCameraMove(CameraPosition position) {
-    //_lastMapPosition = position.target;
+    _lastMapPosition = position.target;
+  }
 
-    print(position);
+  void _onCameraIdle(){
+    _getPlace();
+  }
+
+  void _getPlace() async{
+    List<Placemark> placemarks = await placemarkFromCoordinates(_lastMapPosition.latitude, _lastMapPosition.longitude);
+    String temp = '';
+    temp = placemarks.first.name + ", " + placemarks.first.locality;
+
+    setState(() {
+      _pickedAddress = temp;
+    });
+  }
+
+  void _getCurrentLocation(){
+    _goToCurrentLocation();
+  }
+
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error(
+            'Location permissions are denied');
+      }
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    Position position = await _determinePosition();
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 16,
+    )));
   }
 
 }
